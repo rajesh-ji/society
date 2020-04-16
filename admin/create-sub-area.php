7629ed0b81acc20392210d3becf5ffef6906477a
rajesh-ji <54959742+rajesh-ji@users.noreply.github.com>
2020-04-16 17:00:29 +0530

    dashboard ui updated

---
+++ admin/create-sub-area.php
@@ -0,0 +1,94 @@
+<?php include('include/header.php');
+
+?>
+       
+        <div class="page-wrapper">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card card-body">
+                            <h4 class="card-title">Create Sub Area</h4>                          
+                            <form class="form-horizontal m-t-40">                                                               
+                                <div class="form-group">
+                                    <label>Sub Area Name</label>
+                                    <input type="text" class="form-control" placeholder="area name..">
+                                </div>
+                                 <div class="form-group">
+                                    <label>Image upload</label>
+                                    <input type="file" class="form-control" id="exampleInputFile" aria-describedby="fileHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label>Parent Area Name</label>
+                                    <input type="text" class="form-control" placeholder="area name..">
+                                </div>
+                                <div class="form-group">
+                                    <label>Area Full Details</label>
+                                    <textarea class="form-control" rows="5"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label>Area Status</label>
+                                    <input type="text" class="form-control" placeholder="area status..">
+                                </div>
+                                <button type="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
+                                <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancel</button>
+                            </form>
+                        </div>
+                    </div>
+            </div>        
+        </div>
+                <!-- Row -->
+               
+                <div class="right-sidebar">
+                    <div class="slimscrollright">
+                        <div class="rpanel-title"> Service Panel <span><i class="ti-close right-side-toggle"></i></span> </div>
+                        <div class="r-panel-body">
+                            <ul id="themecolors" class="m-t-20">
+                                <li><b>With Light sidebar</b></li>
+                                <li><a href="javascript:void(0)" data-theme="default" class="default-theme">1</a></li>
+                                <li><a href="javascript:void(0)" data-theme="green" class="green-theme">2</a></li>
+                                <li><a href="javascript:void(0)" data-theme="red" class="red-theme">3</a></li>
+                                <li><a href="javascript:void(0)" data-theme="blue" class="blue-theme working">4</a></li>
+                                <li><a href="javascript:void(0)" data-theme="purple" class="purple-theme">5</a></li>
+                                <li><a href="javascript:void(0)" data-theme="megna" class="megna-theme">6</a></li>
+                                <li class="d-block m-t-30"><b>With Dark sidebar</b></li>
+                                <li><a href="javascript:void(0)" data-theme="default-dark" class="default-dark-theme">7</a></li>
+                                <li><a href="javascript:void(0)" data-theme="green-dark" class="green-dark-theme">8</a></li>
+                                <li><a href="javascript:void(0)" data-theme="red-dark" class="red-dark-theme">9</a></li>
+                                <li><a href="javascript:void(0)" data-theme="blue-dark" class="blue-dark-theme">10</a></li>
+                                <li><a href="javascript:void(0)" data-theme="purple-dark" class="purple-dark-theme">11</a></li>
+                                <li><a href="javascript:void(0)" data-theme="megna-dark" class="megna-dark-theme ">12</a></li>
+                            </ul>
+                            <ul class="m-t-20 chatonline">
+                                <li><b>Chat option</b></li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="../assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>Varun Dhavan <small class="text-success">online</small></span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="../assets/images/users/2.jpg" alt="user-img" class="img-circle"> <span>Genelia Deshmukh <small class="text-warning">Away</small></span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="../assets/images/users/3.jpg" alt="user-img" class="img-circle"> <span>Ritesh Deshmukh <small class="text-danger">Busy</small></span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="../assets/images/users/4.jpg" alt="user-img" class="img-circle"> <span>Arijit Sinh <small class="text-muted">Offline</small></span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="../assets/images/users/5.jpg" alt="user-img" class="img-circle"> <span>Govinda Star <small class="text-success">online</small></span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="../assets/images/users/6.jpg" alt="user-img" class="img-circle"> <span>John Abraham<small class="text-success">online</small></span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="../assets/images/users/7.jpg" alt="user-img" class="img-circle"> <span>Hritik Roshan<small class="text-success">online</small></span></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)"><img src="../assets/images/users/8.jpg" alt="user-img" class="img-circle"> <span>Pwandeep rajan <small class="text-success">online</small></span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            <footer class="footer"> © 2017 Material Pro Admin by wrappixel.com </footer>
+            </div>
+         </div>
+    <?php include('include/footer.php');?>
---
+++ admin/index.php
@@ -0,0 +1,306 @@
+<?php include('include/header.php');
+
+?>
+        <div class="page-wrapper">
+           
+            <div class="container-fluid">
+                <div class="row">
+                    
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card card-body">
+                            
+                            <div class="row">
+                            
+                                <div class="col p-r-0 align-self-center">
+                                    <h2 class="font-light m-b-0">201</h2>
+                                    <h6 class="text-muted">Today Area</h6></div>
+                            
+                                <!-- <div class="col text-right align-self-center">
+                                    <div data-label="20%" class="css-bar m-b-0 css-bar-info css-bar-20"></div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card card-body">
+                           
+                            <div class="row">
+                                
+                                <div class="col p-r-0 align-self-center">
+                                    <h2 class="font-light m-b-0">276</h2>
+                                    <h6 class="text-muted">Total Adv.</h6></div>
+                                
+                                <!-- <div class="col text-right align-self-center">
+                                    <div data-label="30%" class="css-bar m-b-0 css-bar-success css-bar-20"></div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card card-body">
+                     
+                            <div class="row">
+                     
+                                <div class="col p-r-0 align-self-center">
+                                    <h2 class="font-light m-b-0">195</h2>
+                                    <h6 class="text-muted">Today Contacts</h6></div>
+                     
+                               <!--  <div class="col text-right ">
+                                    <div data-label="40%" class="css-bar m-b-0 css-bar-primary css-bar-40"></div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card card-body">
+                    
+                            <div class="row">
+                    
+                                <div class="col p-r-0 align-self-center">
+                                    <h2 class="font-light m-b-0">6870</h2>
+                                    <h6 class="text-muted">Total People</h6></div>
+                    
+                               <!--  <div class="col text-right align-self-center">
+                                    <div data-label="60%" class="css-bar m-b-0 css-bar-danger css-bar-60"></div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                 <div class="row">
+                    
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card card-body">
+                            
+                            <div class="row">
+                            
+                                <div class="col p-r-0 align-self-center">
+                                    <h2 class="font-light m-b-0">324</h2>
+                                    <h6 class="text-muted">Today Events</h6></div>
+                            
+                               <!--  <div class="col text-right align-self-center">
+                                    <div data-label="20%" class="css-bar m-b-0 css-bar-info css-bar-20"></div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card card-body">
+                           
+                            <div class="row">
+                                
+                                <div class="col p-r-0 align-self-center">
+                                    <h2 class="font-light m-b-0">2376</h2>
+                                    <h6 class="text-muted">Total Blood Dooner</h6></div>
+                                
+                               <!--  <div class="col text-right align-self-center">
+                                    <div data-label="30%" class="css-bar m-b-0 css-bar-success css-bar-20"></div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card card-body">
+                     
+                            <div class="row">
+                     
+                                <div class="col p-r-0 align-self-center">
+                                    <h2 class="font-light m-b-0">1795</h2>
+                                    <h6 class="text-muted">Today Blogs</h6></div>
+                     
+                               <!--  <div class="col text-right ">
+                                    <div data-label="40%" class="css-bar m-b-0 css-bar-primary css-bar-40"></div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6 col-lg-3">
+                        <div class="card card-body">
+                    
+                            <div class="row">
+                    
+                                <div class="col p-r-0 align-self-center">
+                                    <h2 class="font-light m-b-0">36870</h2>
+                                    <h6 class="text-muted">Total Matrimonial</h6>
+                                </div>
+                    
+                               <!--  <div class="col text-right align-self-center">
+                                    <div data-label="60%" class="css-bar m-b-0 css-bar-danger css-bar-60"></div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                     <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                            <button type="button" class="btn btn-success" style="float:right;">View All</button>
+                                <h4 class="card-title">Recent Add Contacts</h4>
+                                <div class="table-responsive m-t-20">
+                                    <table class="table stylish-table">
+                                        <thead>
+                                            <tr>
+                                                <th colspan="2">Name</th>                                             
+                                                <th>Date</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                       <tbody>
+                                            <tr>
+                                                <td style="width:50px;"><span class="round">S</span></td>
+                                                <td>
+                                                    <h6>Sunil Joshi</h6><small class="text-muted">Web Designer</small></td>
+                                               
+                                                <td>$3.9K</td>
+                                            </tr>
+                                            <tr class="active">
+                                                <td><span class="round"><img src="../assets/images/users/2.jpg" alt="user" width="50" /></span></td>
+                                                <td>
+                                                    <h6>Andrew</h6><small class="text-muted">Project Manager</small></td>
+                                               
+                                            </tr>
+                                            <tr>
+                                                <td><span class="round round-success">B</span></td>
+                                                <td>
+                                                    <h6>Bhavesh patel</h6><small class="text-muted">Developer</small></td>
+                                               
+                                                <td>$12.9K</td>
+                                            </tr>
+                                            <tr>
+                                                <td><span class="round round-primary">N</span></td>
+                                                <td>
+                                                    <h6>Nirav Joshi</h6><small class="text-muted">Frontend Eng</small></td>
+                                               
+                                                <td>$10.9K</td>
+                                            </tr>
+                                            
+                                           
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                            <button type="button" class="btn btn-success" style="float:right;">View All</button>
+                                <h4 class="card-title">Recent Add Event</h4>
+                                <div class="table-responsive m-t-20">
+                                    <table class="table stylish-table">
+                                        <thead>
+                                            <tr>
+                                                <th colspan="2">Event Name</th>
+                                               
+                                                <th>Date</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td style="width:50px;"><span class="round">S</span></td>
+                                                <td>
+                                                    <h6>Sunil Joshi</h6><small class="text-muted">Web Designer</small></td>
+                                               
+                                                <td>$3.9K</td>
+                                            </tr>
+                                            <tr class="active">
+                                                <td><span class="round"><img src="../assets/images/users/2.jpg" alt="user" width="50" /></span></td>
+                                                <td>
+                                                    <h6>Andrew</h6><small class="text-muted">Project Manager</small></td>
+                                               
+                                            </tr>
+                                            <tr>
+                                                <td><span class="round round-success">B</span></td>
+                                                <td>
+                                                    <h6>Bhavesh patel</h6><small class="text-muted">Developer</small></td>
+                                               
+                                                <td>$12.9K</td>
+                                            </tr>
+                                            <tr>
+                                                <td><span class="round round-primary">N</span></td>
+                                                <td>
+                                                    <h6>Nirav Joshi</h6><small class="text-muted">Frontend Eng</small></td>
+                                               
+                                                <td>$10.9K</td>
+                                            </tr>
+                                            
+                                           
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                      <div class="card">
+                            <div class="card-body">
+                            <button type="button" class="btn btn-success" style="float:right;">View All</button>
+                                <h4 class="card-title">Recent Coming Event</h4>
+                                <div class="table-responsive m-t-20">
+                                    <table class="table stylish-table">
+                                        <thead>
+                                            <tr>
+                                                <th colspan="2">Event Name</th>
+                                               
+                                                <th>Date</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td style="width:50px;"><span class="round">S</span></td>
+                                                <td>
+                                                    <h6>Sunil Joshi</h6><small class="text-muted">Web Designer</small></td>
+                                               
+                                                <td>$3.9K</td>
+                                            </tr>
+                                            <tr class="active">
+                                                <td><span class="round"><img src="../assets/images/users/2.jpg" alt="user" width="50" /></span></td>
+                                                <td>
+                                                    <h6>Andrew</h6><small class="text-muted">Project Manager</small></td>
+                                               
+                                            </tr>
+                                            <tr>
+                                                <td><span class="round round-success">B</span></td>
+                                                <td>
+                                                    <h6>Bhavesh patel</h6><small class="text-muted">Developer</small></td>
+                                               
+                                                <td>$12.9K</td>
+                                            </tr>
+                                            <tr>
+                                                <td><span class="round round-primary">N</span></td>
+                                                <td>
+                                                    <h6>Nirav Joshi</h6><small class="text-muted">Frontend Eng</small></td>
+                                               
+                                                <td>$10.9K</td>
+                                            </tr>
+                                            
+                                           
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- Row -->
+                <div class="row">
+                    <div class="col-lg-4 col-md-12">
+                    </div>
+                    <div class="col-lg-4 col-md-12">
+                    </div>
+                </div>
+                
+            <footer class="footer"> © 2017 Material Pro Admin by wrappixel.com </footer>
+            </div>
+         </div>
+    <?php include('include/footer.php');?>
